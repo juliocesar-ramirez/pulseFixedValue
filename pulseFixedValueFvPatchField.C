f8dd1e4d9bf562f4b5b63573e31021f744eeff84
juliocesar-ramirez <julio.ramirez@pucp.pe>
2022-07-21 22:07:32 -0500

    parametros de la ecuacion

--- pulseFixedValueFvPatchField.C
+++ pulseFixedValueFvPatchField.C
@@ -23,11 +23,13 @@ License
 
 \*---------------------------------------------------------------------------*/
 
+#include "direction.H"
 #include "pulseFixedValueFvPatchField.H"
 #include "addToRunTimeSelectionTable.H"
 #include "fvPatchFieldMapper.H"
 #include "volFields.H"
 #include "surfaceFields.H"
+#include <cmath>
 
 // * * * * * * * * * * * * * Private Member Functions  * * * * * * * * * * * //
 
@@ -37,164 +39,114 @@ Foam::scalar Foam::pulseFixedValueFvPatchField<Type>::t() const
     return this->db().time().timeOutputValue();
 }
 
+template<class Type>
+Foam::scalar Foam::pulseFixedValueFvPatchField<Type>::pulseFraction() const
+{
+    scalar duracionFraction=duracion_/periodo_;
+    scalar cycleFraction=fmod(t()/periodo_,1);
+    if (cycleFraction>duracionFraction) {
+        return 0.0;;
+    }
+    else {
+        return 1.0;
+    }
+
+}
 
 // * * * * * * * * * * * * * * * * Constructors  * * * * * * * * * * * * * * //
 
-template<class Type>
-Foam::pulseFixedValueFvPatchField<Type>::
-pulseFixedValueFvPatchField
-(
-    const fvPatch& p,
-    const DimensionedField<Type, volMesh>& iF
-)
-:
-    fixedValueFvPatchField<Type>(p, iF),
-    scalarData_(0.0),
-    data_(Zero),
-    fieldData_(p.size(), Zero),
-    timeVsData_(),
-    wordData_("wordDefault"),
-    labelData_(-1),
-    boolData_(false)
-{
+template <class Type>
+Foam::pulseFixedValueFvPatchField<Type>::pulseFixedValueFvPatchField(
+    const fvPatch &p, const DimensionedField<Type, volMesh> &iF)
+    : fixedValueFvPatchField<Type>(p, iF), periodo_(0.0),duracion_(0.0), baseValue_(Zero),
+      pulseValue_(p.size(), Zero) {}
+
+template <class Type>
+Foam::pulseFixedValueFvPatchField<Type>::pulseFixedValueFvPatchField(
+    const fvPatch &p, const DimensionedField<Type, volMesh> &iF,
+    const dictionary &dict)
+    : fixedValueFvPatchField<Type>(p, iF),
+      periodo_(dict.lookup<scalar>("periodo")),
+      duracion_(dict.lookup<scalar>("duracion")),
+      baseValue_(dict.lookupOrDefault<Type>("baseValue", Zero)),
+      pulseValue_("pulseValue", dict, p.size()) {
+
+  fixedValueFvPatchField<Type>::evaluate();
+
+  /*
+  // Initialise with the value entry if evaluation is not possible
+  fvPatchField<Type>::operator=
+  (
+      Field<Type>("value", dict, p.size())
+  );
+  */
 }
 
+template <class Type>
+Foam::pulseFixedValueFvPatchField<Type>::pulseFixedValueFvPatchField(
+    const pulseFixedValueFvPatchField<Type> &ptf, const fvPatch &p,
+    const DimensionedField<Type, volMesh> &iF, const fvPatchFieldMapper &mapper)
+    : fixedValueFvPatchField<Type>(ptf, p, iF, mapper), periodo_(ptf.periodo_),duracion_(ptf.duracion_),
+      baseValue_(ptf.baseValue_), pulseValue_(mapper(ptf.pulseValue_)) {}
 
-template<class Type>
-Foam::pulseFixedValueFvPatchField<Type>::
-pulseFixedValueFvPatchField
-(
-    const fvPatch& p,
-    const DimensionedField<Type, volMesh>& iF,
-    const dictionary& dict
-)
-:
-    fixedValueFvPatchField<Type>(p, iF),
-    scalarData_(dict.lookup<scalar>("scalarData")),
-    data_(dict.lookup<Type>("data")),
-    fieldData_("fieldData", dict, p.size()),
-    timeVsData_(Function1<Type>::New("timeVsData", dict)),
-    wordData_(dict.lookupOrDefault<word>("wordName", "wordDefault")),
-    labelData_(-1),
-    boolData_(false)
-{
-
-
-    fixedValueFvPatchField<Type>::evaluate();
-
-    /*
-    // Initialise with the value entry if evaluation is not possible
-    fvPatchField<Type>::operator=
-    (
-        Field<Type>("value", dict, p.size())
-    );
-    */
-}
-
-
-template<class Type>
-Foam::pulseFixedValueFvPatchField<Type>::
-pulseFixedValueFvPatchField
-(
-    const pulseFixedValueFvPatchField<Type>& ptf,
-    const fvPatch& p,
-    const DimensionedField<Type, volMesh>& iF,
-    const fvPatchFieldMapper& mapper
-)
-:
-    fixedValueFvPatchField<Type>(ptf, p, iF, mapper),
-    scalarData_(ptf.scalarData_),
-    data_(ptf.data_),
-    fieldData_(mapper(ptf.fieldData_)),
-    timeVsData_(ptf.timeVsData_, false),
-    wordData_(ptf.wordData_),
-    labelData_(-1),
-    boolData_(ptf.boolData_)
-{}
-
-
-template<class Type>
-Foam::pulseFixedValueFvPatchField<Type>::
-pulseFixedValueFvPatchField
-(
-    const pulseFixedValueFvPatchField<Type>& ptf,
-    const DimensionedField<Type, volMesh>& iF
-)
-:
-    fixedValueFvPatchField<Type>(ptf, iF),
-    scalarData_(ptf.scalarData_),
-    data_(ptf.data_),
-    fieldData_(ptf.fieldData_),
-    timeVsData_(ptf.timeVsData_, false),
-    wordData_(ptf.wordData_),
-    labelData_(-1),
-    boolData_(ptf.boolData_)
-{}
-
+template <class Type>
+Foam::pulseFixedValueFvPatchField<Type>::pulseFixedValueFvPatchField(
+    const pulseFixedValueFvPatchField<Type> &ptf,
+    const DimensionedField<Type, volMesh> &iF)
+    : fixedValueFvPatchField<Type>(ptf, iF), periodo_(ptf.periodo_),duracion_(ptf.duracion_),
+      baseValue_(ptf.baseValue_), pulseValue_(ptf.pulseValue_) {}
 
 // * * * * * * * * * * * * * * * Member Functions  * * * * * * * * * * * * * //
 
-template<class Type>
-void Foam::pulseFixedValueFvPatchField<Type>::autoMap
-(
-    const fvPatchFieldMapper& m
-)
-{
-    fixedValueFvPatchField<Type>::autoMap(m);
-    m(fieldData_, fieldData_);
+template <class Type>
+void Foam::pulseFixedValueFvPatchField<Type>::autoMap(
+    const fvPatchFieldMapper &m) {
+  fixedValueFvPatchField<Type>::autoMap(m);
+  m(pulseValue_, pulseValue_);
 }
 
-
-template<class Type>
-void Foam::pulseFixedValueFvPatchField<Type>::rmap
-(
+      template<class Type>
+      void Foam::pulseFixedValueFvPatchField<Type>::rmap
+      (
     const fvPatchField<Type>& ptf,
     const labelList& addr
 )
-{
-    fixedValueFvPatchField<Type>::rmap(ptf, addr);
+      {
+  fixedValueFvPatchField<Type>::rmap(ptf, addr);
 
-    const pulseFixedValueFvPatchField<Type>& tiptf =
-        refCast<const pulseFixedValueFvPatchField<Type>>(ptf);
+  const pulseFixedValueFvPatchField<Type> &tiptf =
+      refCast<const pulseFixedValueFvPatchField<Type>>(ptf);
 
-    fieldData_.rmap(tiptf.fieldData_, addr);
-}
-
-
-template<class Type>
-void Foam::pulseFixedValueFvPatchField<Type>::updateCoeffs()
-{
-    if (this->updated())
-    {
-        return;
-    }
-
-    fixedValueFvPatchField<Type>::operator==
-    (
-        data_
-      + fieldData_
-      + scalarData_*timeVsData_->value(t())
-    );
+  pulseValue_.rmap(tiptf.pulseValue_, addr);
+      }
 
 
-    fixedValueFvPatchField<Type>::updateCoeffs();
-}
+      template<class Type>
+      void Foam::pulseFixedValueFvPatchField<Type>::updateCoeffs()
+      {
+  if (this->updated()) {
+    return;
+  }
+
+  fixedValueFvPatchField<Type>::operator==(baseValue_ + pulseValue_);
+
+  fixedValueFvPatchField<Type>::updateCoeffs();
+      }
 
 
-template<class Type>
-void Foam::pulseFixedValueFvPatchField<Type>::write
-(
+      template<class Type>
+      void Foam::pulseFixedValueFvPatchField<Type>::write
+      (
     Ostream& os
 ) const
-{
-    fvPatchField<Type>::write(os);
-    writeEntry(os, "scalarData", scalarData_);
-    writeEntry(os, "data", data_);
-    writeEntry(os, "fieldData", fieldData_);
-    writeEntry(os, timeVsData_());
-    writeEntry(os, "wordData", wordData_);
-    writeEntry(os, "value", *this);
-}
+      {
+  fvPatchField<Type>::write(os);
+  writeEntry(os, "periodo", periodo_);
+  writeEntry(os, "duracion", duracion_);
+  writeEntry(os, "baseValue", baseValue_);
+  writeEntry(os, "pulseValue", pulseValue_);
+  writeEntry(os, "value", *this);
+      }
 
 
 
